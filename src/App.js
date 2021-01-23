712bc9cf1dec718fe7167c4d9340261de9b045ec
Dharmesh Patel <dharmeshbbay@gmail.com>
2021-01-23 11:02:00 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    My App
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React With Auth
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default withAuthenticator(App);
+export default App;
+
+// import React from "react";
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+// const App = () => (
+//   <div>
+//     <AmplifySignOut />
+//     My App
+//   </div>
+// );
+
+// export default withAuthenticator(App);
 // import React from "react";
 // // import logo from "./logo.svg";
 // // import "./App.css";
